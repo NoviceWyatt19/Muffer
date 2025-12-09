8035bc3f79f176e47097e073b4c9418f2ff831ed
Wyatt <aseltula596@gmail.com>
2025-12-09 16:53:40 +0900

    initial - project initial commit

---
+++ src/main/java/io/github/wyatt/muffer/MufferApplication.java
@@ -0,0 +1,13 @@
+package io.github.wyatt.muffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MufferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MufferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/wyatt/muffer/MufferApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wyatt.muffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MufferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
