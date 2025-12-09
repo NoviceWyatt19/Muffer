42c565488cd81941f56a60db81043788374c40d8
Wyatt <aseltula596@gmail.com>
2025-12-09 18:37:47 +0900

    chore: rename App.java and update .gitignore

--- src/main/java/io/github/wyatt/muffer/App.java
+++ src/main/java/io/github/wyatt/muffer/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MufferApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MufferApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
