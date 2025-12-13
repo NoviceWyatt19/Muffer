caab10c209e546411d6da834fcf57ab6dd10f672
Wyatt <aseltula596@gmail.com>
2025-12-13 23:57:12 +0900

    test: BoardService 통합테스트

--- src/test/java/io/github/wyatt/muffer/domain/board/BoardServiceTest.java
+++ src/test/java/io/github/wyatt/muffer/domain/board/BoardServiceTest.java
@@ -1,22 +1,85 @@
 package io.github.wyatt.muffer.domain.board;
 
+import io.github.wyatt.muffer.domain.board.code.BoardStatus;
+import io.github.wyatt.muffer.domain.board.code.ChargeType;
+import io.github.wyatt.muffer.domain.board.code.ProductCategory;
+import io.github.wyatt.muffer.domain.board.code.QualityGrade;
+import io.github.wyatt.muffer.domain.board.entity.Board;
+import io.github.wyatt.muffer.domain.board.entity.Product;
+import io.github.wyatt.muffer.domain.board.repository.BoardRepo;
+import io.github.wyatt.muffer.domain.board.repository.ProductRepo;
 import io.github.wyatt.muffer.domain.board.request.BoardFilterReq;
+import io.github.wyatt.muffer.domain.board.response.BoardListRes;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
 class BoardServiceTest {
 
     @Autowired
-    private BoardService boardService;
+    BoardService boardService;
+
+    @Autowired
+    BoardRepo boardRepo;
+
+    @Autowired
+    ProductRepo productRepo;
+
+
+    @BeforeEach
+    void setUp() {
+        Product product = Product.create(
+                "test name",
+                ProductCategory.HEADPHONE,
+                QualityGrade.A,
+                "serial code",
+                false,
+                4,
+                false,
+                null,
+                "3.5mm",
+                ChargeType.None,
+                null,
+                null,
+                60.5f,
+                true,
+                false
+        );
+        Board board = Board.create(
+                "test title",
+                BoardStatus.SELLING,
+                product,
+                1,
+                "test brand name",
+                1000,
+                3
+        );
+
+        productRepo.save(product);
+        boardRepo.save(board);
+    }
 
     @Test
     @DisplayName("Find All Boards")
     void findAllTest() {
         BoardFilterReq req = new BoardFilterReq();
-        boardService.getBoards(req);
+        List<BoardListRes> res = boardService.getBoards(req);
+        System.out.println(res.toString());
+        assertThat(res.isEmpty(), is(false));
+        assertThat(res.size(), is(1));
     }
 
 }
