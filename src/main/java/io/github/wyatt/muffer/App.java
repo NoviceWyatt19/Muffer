32ead42ed937b1c7882e2d230f6f950db00fa6d8
Wyatt <aseltula596@gmail.com>
2025-12-14 16:33:48 +0900

    chore: JpaAuditing 활성화

--- src/main/java/io/github/wyatt/muffer/App.java
+++ src/main/java/io/github/wyatt/muffer/App.java
@@ -2,7 +2,9 @@ package io.github.wyatt.muffer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class App {
 
