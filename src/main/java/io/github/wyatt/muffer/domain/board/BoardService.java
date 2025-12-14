80e1a9385781abbf1876c0d2100e9d7f4bc8c0fc
Wyatt <aseltula596@gmail.com>
2025-12-14 16:33:27 +0900

    refactor: 조회 시 상품정보 추가

--- src/main/java/io/github/wyatt/muffer/domain/board/BoardService.java
+++ src/main/java/io/github/wyatt/muffer/domain/board/BoardService.java
@@ -6,6 +6,7 @@ import io.github.wyatt.muffer.domain.board.repository.ProductRepo;
 import io.github.wyatt.muffer.domain.board.request.BoardFilterReq;
 import io.github.wyatt.muffer.domain.board.request.BoardSvReq;
 import io.github.wyatt.muffer.domain.board.response.BoardListRes;
+import io.github.wyatt.muffer.domain.option.OptionRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,12 +20,21 @@ import java.util.List;
 @Slf4j
 public class BoardService {
 
-    private final BoardRepo bRepo;
-    private final ProductRepo pRepo;
+    private final BoardRepo brdRepo;
+    private final ProductRepo pdtRepo;
+    private  final OptionRepo optRepo;
 
     public List<BoardListRes> getBoards(BoardFilterReq res) {
-        List<Board> boards = bRepo.findAll();
-        return boards.stream().map(BoardListRes::from).toList();
+        List<Board> boards = brdRepo.findAll();
+
+        return boards.stream().map( (board) -> BoardListRes.from(board, getBrand(board.getProduct().getBrandOptionId(),  board.getProduct().getCustomBrand())) ).toList();
+    }
+
+    private String getBrand(Integer brandId, String customBrand) {
+
+        String brandName = optRepo.findNameById(brandId).orElse(customBrand);
+        log.info("brand name -> {}", brandName);
+        return brandName;
     }
 
     //TODO memberId 적용
@@ -32,9 +42,9 @@ public class BoardService {
     public void saveBoard(BoardSvReq req, int memberId) {
         log.debug("save board info -> {}", req.toString());
         var product = req.extractProduct();
-        pRepo.save(product);
-        bRepo.save(
-                req.toEntity(product)
+        pdtRepo.save(product);
+        brdRepo.save(
+                req.toEntity(product, memberId)
         );
     }
 
