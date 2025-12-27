50a2007b3c2541cb37342fb6183fe94b6b2d09eb
Wyatt <aseltula596@gmail.com>
2025-12-28 02:01:32 +0900

    fix: 로컬 개발 환경 설정

--- src/main/java/io/github/wyatt/muffer/global/auth/utils/TokenCookieFactory.java
+++ src/main/java/io/github/wyatt/muffer/global/auth/utils/TokenCookieFactory.java
@@ -2,13 +2,14 @@ package io.github.wyatt.muffer.global.auth.utils;
 
 import org.springframework.http.ResponseCookie;
 
+//NOTE 로컬 개발 설정: secure(false), sameSite("Lax") / 배포 환경 설정: secure(true), sameSite("None")
 public class TokenCookieFactory {
     public static ResponseCookie createTokenCookie(String name, String value, Long expiration) {
         return ResponseCookie.from(name, value)
                 .path("/")
-                .httpOnly(true) // xss 방지
-                .secure(false) // https 강제
-                .sameSite("None") // cors 방지
+                .httpOnly(true) // js 접근 차단 여부
+                .secure(false) // https 강제 여부
+                .sameSite("Lax") // csrf
                 .maxAge(expiration)
                 .build();
     }
@@ -17,7 +18,7 @@ public class TokenCookieFactory {
                 .path("/")
                 .httpOnly(true)
                 .secure(false)
-                .sameSite("None")
+                .sameSite("Lax")
                 .maxAge(0)
                 .build();
     }
